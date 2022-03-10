af76d6c6851073c8eca146222cf16dc92fc97b95
wsmagacki <waldemar.smagacki@gmail.com>
2022-03-10 23:57:11 +0100

    ver 3

--- src/App.java
+++ src/App.java
@@ -4,25 +4,69 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        String[] txt = {"zero", "jeden", "dwa", "trzy", "cztery", "pięć", "sześć", "siedem", "osiem", "dziewięć",
-                "dziesięć", "jedenaście", "dwanaście", "trzynaście", "czternaście", "piętnaście", "szesnaście",
-                "siedemnaście", "osiemnaście", "dziewiętnaście"};
+        String[] ones = {"jeden", "dwa", "trzy", "cztery", "pięć", "sześć", "siedem", "osiem",
+                "dziewięć", "dziesięć", "jedenaście", "dwanaście", "trzynaście", "czternaście",
+                "piętnaście", "szesnaście", "siedemnaście", "osiemnaście", "dziewiętnaście"};
+
+        String[] tens = {"dwadzieścia", "trzydzieści", "czterdzieści", "pięćdziesiąt", "sześćdziesiąt",
+                "siedemdziesiąt", "osiemdziesiąt", "dziewięćdziesiąt"};
+
+        String[] hundreds = {"sto", "dwieście", "trzysta", "czterysta", "pięcset",
+                "sześćset", "siedemset", "osiemset", "dziewięćset"};
 
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
-        StringBuilder slownie = new StringBuilder("");
-        String liczbaStr = Integer.toString(x);
+        StringBuilder result = new StringBuilder("");
+        String numberStr = Integer.toString(x);
 
-        List<String> trojki = new ArrayList<>();
+        List<String> threes = generateListOfThrees(numberStr);
+        System.out.println(threes);
 
+        String three = threes.get(0);
+        int digit;
+        String rest = "";
 
-        String dwieOstatnie = liczbaStr.substring(1, 3);
-        int dwieOstatnieInt = Integer.parseInt(dwieOstatnie);
-        if (dwieOstatnieInt < 20) {
-            slownie.append(" ").append(txt[dwieOstatnieInt]);
+        if (three.length() == 3) {
+            if(!three.startsWith("0")) {
+                digit = Integer.parseInt(three.substring(0, 1));
+                result.append(hundreds[digit - 1]);
+                rest = three.substring(1);
+            }
+        } else {
+            rest = three;
         }
 
-        System.out.println(slownie);
+        if (Integer.parseInt(rest) < 20) {
+            digit = Integer.parseInt(rest);
+        } else {
+            digit = Integer.parseInt(rest.substring(0, 1));
+            result.append(" ").append(tens[digit - 2]);
+            digit = Integer.parseInt(rest.substring(1));
+        }
+        result.append(" ").append(digit > 0 ? ones[digit - 1] : "");
 
+        System.out.println(result.toString().trim());
+    }
+
+    static List<String> generateListOfThrees(String number) {
+        List<String> threes = new ArrayList<>();
+        int len;
+        String part;
+
+        while (!number.equals("")) {
+            len = number.length();
+
+            if (len <= 3) {
+                part = number;
+                number = "";
+            } else {
+                part = number.substring(len - 3);
+                number = number.substring(0, len - 3);
+            }
+
+            threes.add(part);
+        }
+
+        return threes;
     }
 }
